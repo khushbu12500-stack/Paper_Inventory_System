26126ac901f6a1e953501f2bda1ff691c9fd4aec
Khushbu Kumari <khushbusingh887258@gmail.com>
2026-07-17 12:31:03 +0530

    Updated database configuration for deployment

--- database.py
+++ database.py
@@ -1,10 +1,12 @@
+import os
 import mysql.connector
 
 conn = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="khushbu@060626",
-    database="paper_inventory_db"
+    host=os.getenv("DB_HOST", "localhost"),
+    user=os.getenv("DB_USER", "root"),
+    password=os.getenv("DB_PASSWORD", "khushbu@060626"),
+    database=os.getenv("DB_NAME", "paper_inventory_db"),
+    port=int(os.getenv("DB_PORT", 3306))
 )
 
 cursor = conn.cursor(buffered=True)
